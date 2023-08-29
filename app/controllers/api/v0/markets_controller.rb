1a88d68782d41c17c921d873b67cf6a8465bfaf0
Davis Weimer <davisweimer@gmail.com>
2023-08-29 13:36:19 -0600

    test & feat: /api/v0/markets/#{id}

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -2,4 +2,12 @@ class Api::V0::MarketsController < ApplicationController
   def index
     render json: { data: MarketSerializer.format_markets(Market.all) }
   end
+
+  def show
+    begin
+      render json: { data: MarketSerializer.format_market(Market.find(params[:id])) }
+    rescue StandardError => e
+      render json: { errors: [{ detail: e.message }] }, status: 404
+    end
+  end
 end
