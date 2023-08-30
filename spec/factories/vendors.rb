824af86a44025fa5778ca1605005b4af49bbeada
Davis Weimer <davisweimer@gmail.com>
2023-08-30 10:39:15 -0600

    feat: UPDATE Vendor & sad paths

--- spec/factories/vendors.rb
+++ spec/factories/vendors.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :vendor do
-    name { "#{[Faker::Superhero.suffix, Faker::Science.modifier].sample} #{Faker::Restaurant.name}" }
+    name { "#{[Faker::Cannabis.buzzword.capitalize, Faker::Science.modifier, Faker::Verb.past_participle.capitalize].sample} #{Faker::Restaurant.name}" }
     description { Faker::Restaurant.description }
     contact_name { "#{Faker::Emotion.adjective.capitalize} #{Faker::Name.first_name}" }
     contact_phone { Faker::PhoneNumber.cell_phone }
